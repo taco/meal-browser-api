086318f2aad685dae746ce549f22639e07e3ad3c
Taco <travis_johnson@volusion.com>
2015-08-05 08:43:58 -0500

    Meal create now returns a full list of filtered meals

--- src/routes.js
+++ src/routes.js
@@ -41,12 +41,28 @@ module.exports = function(app) {
 			if (this.request.accepts('json') !== 'json')
 				this.throw(406, 'json only');
 
-			
+			var offset = this.request.query.offset || 0;
+			var limit = this.request.query.limit || 20;
+			var sortby = this.request.query.sortby || 'rating';
+			var sortdir = this.request.query.sortdir || 'desc';
 
-			this.body = {
+			var filter = []
+			try {
+				filter = JSON.parse(this.request.query.filter)
+			} catch (e) {}
+
+			this.set('X-Content-Type-Options', 'nosniff');
+			if (this.query.callback) {
+				this.set('Content-Type', 'text/javascript');
+			} else {
+				this.set('Content-Type', 'application/json');
+			}
+
+			this.body = jsonp({
 				success: true,
-				meal: meals.add(this.request.body)
-			};
+				meal: meals.add(this.request.body),
+				items: meals.list(offset, limit, sortby, sortdir, filter)
+			});
 		})
 		.put('/meals/:id', function*(next) {
 			meals.update(this.params.id, this.request.body);
